e1aee3b02df5eed00dd386cb85fa0172a1a92e5f
wxw <996268132@qq.com>
2026-01-06 18:24:15 +0800

    update proto

--- proto.bat
+++ proto.bat
@@ -1,4 +1,7 @@
 ::protoc --go_out=internal/opspb --go_opt=paths=source_relative --connect-go_out=internal/opspb --connect-go_opt=paths=source_relative protos/ops/ops.proto
 
-protoc --go_out=. --go_opt=module=goakt-actors-cluster protos/ops/ops.proto
-protoc --connect-go_out=. --connect-go_opt=module=goakt-actors-cluster protos/ops/ops.proto
+::protoc --go_out=. --go_opt=module=goakt-actors-cluster protos/ops/ops.proto
+::protoc --connect-go_out=. --connect-go_opt=module=goakt-actors-cluster protos/ops/ops.proto
+
+protoc --proto_path=protos/ops --go_out=internal/opspb --go_opt=paths=source_relative --connect-go_out=internal/opspb --connect-go_opt=paths=source_relative ops.proto
+protoc --proto_path=protos/sample --go_out=internal/samplepb --go_opt=paths=source_relative --connect-go_out=internal/samplepb --connect-go_opt=paths=source_relative sample.proto service.proto
